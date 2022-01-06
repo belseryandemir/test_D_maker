b6548e99233725dff8e3ef32c9ca717f08a48270
demir <92115677+belseryandemir@users.noreply.github.com>
2022-01-06 16:57:40 +0300

    my_bot

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-from telegram import *
-from telegram.ext import *
-from requests import *
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.update import Update
+import settings
 
-updater = Updater(token="5098782966:AAHGj39Ay3oWAIpGG1Ty5faJRUbteQwXdP4")
+updater = Updater(token=settings.TELEGRAM_TOKEN)
+
+def start(update: Update, context:CallbackContext):
+    update.message.reply_text('Hi there')
+
 dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
 
-someMusicOnPC = "browse on your pc"
-someMusicOnInter = "search the music"
-
-someMusicOnPCUrl = "https://audioalter.com/preset/8d-audio"
-someMusicOnInterUrl = "https://www.shazam.com/ru"
-
-
-def startCommand(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton(someMusicOnPC)], [KeyboardButton(someMusicOnInter)]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Ur welcome",
-                             reply_markup=ReplyKeyboardMarkup(buttons))
-
-
-def messageHandler(update: Update, context: CallbackContext):
-    if someMusicOnPC in update.message.text:
-        music = get(someMusicOnPCUrl).content
-    if someMusicOnInter in update.message.text:
-        music = get(someMusicOnInterUrl).content
-
-    if music:
-        context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaMusic
-                                                                            (music, caption="")])
-
-
-dispatcher.add_handler(CommandHandler("start", startCommand))
-dispatcher.add_handler(MessageHandler(Filters.text, MessageHandler))
-
-updater.start_webhook()
+updater.start_polling()
+updater.idle()
